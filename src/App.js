fc6c77093399d5b26312e711a62d2d1e3d873346
Nathan Hodnett <nathanhodnett@gmail.com>
2022-05-15 14:47:31 -0600

    Add error handling

--- src/App.js
+++ src/App.js
@@ -15,35 +15,56 @@ class App extends Component{
     }
   }
 
-  handleClick = (id) => {
-    // console.log(id)
-    {!!id ? fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data);
-      this.setState({movieSelected: true, movieDetails: data.movie})})
-    :
-      this.setState({movieSelected: false})
-    // fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    // .then(response => response.json())
-    // .then(data => this.setState({movieSelected: false, movies: data.movies}))
-    }
-  }
-    // this.setState({
-    //   movieSelected: id
-    // console.log(this.state)
-  //}
-
   componentDidMount = () => {
     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
+    .then(response => {
+      // this.getErrorMessage(response.status)
+      if(response.status === 200) {
+        return response.json()
+      }
+      throw new Error(this.getErrorMessage(response.status))})
     .then(data => this.setState({movies: data.movies}))
+    .catch(err => alert(err))
     //this.setState({ movies: moviesData.movies })
   }
+
+  handleClick = (id) => {
+    {!!id ? fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(response => {
+      // this.getErrorMessage(response.status)
+      if(response.status === 200) {
+        return response.json()
+      }
+      throw new Error(this.getErrorMessage(response.status))})
+    .then(data => {
+      this.setState({movieSelected: true, movieDetails: data.movie})})
+    .catch(err => alert(err))
+    :
+      this.setState({movieSelected: false})
+    }
+  }
+
+  getErrorMessage = (status) => {
+    switch(true) {
+      case (status >= 300 && status <= 399):
+      return 'Redirection message'
+      break;
+      case (status >= 400 && status <= 499):
+      return 'Client error'
+      break;
+      case (status >= 500):
+      return 'Server error'
+      break;
+      default:
+      return 'I have no idea what this error message is for...';
+    }
+    //let message = `Something went wrong while obtaining data! Try again later...`;
+  }
+
   //
   // componentDidUpdate = () => {
   // }
-  //
+
   // componentWillUnmount = () => {
   // }
 
