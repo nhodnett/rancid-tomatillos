17f846bce43e3febefebd856d5bd0249b213ed6d
Eli Davidson <eli@shadowlab.dev>
2022-05-17 19:42:00 -0600

    Refactor

--- src/App.js
+++ src/App.js
@@ -18,52 +18,6 @@ class App extends Component{
     }
   }
 
-  // componentDidMount = () => {
-  //   fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  //   .then(response => {
-  //     if(response.status === 200) {
-  //       return response.json()
-  //     }
-  //     throw new Error(this.getErrorMessage(response.status))})
-  //   .then(data => this.setState({movies: data.movies}))
-  //   .catch(error => {
-  //     this.setState({errorMessage: error.message})
-  //   })
-  // }
-
-  // handleClick = (id) => {
-  //   {!!id ? fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-  //   .then(response => {
-  //     if(response.status === 200) {
-  //       return response.json()
-  //     }
-  //     throw new Error(this.getErrorMessage(response.status))})
-  //   .then(data => {
-  //     this.setState({movieSelected: true, movieDetails: data.movie})})
-  //   .catch(error => {
-  //     this.setState({errorMessage: error.message})
-  //   })
-  //   :
-  //     this.setState({movieSelected: false, errorMessage: ''})
-  //   }
-  // }
-
-  // getErrorMessage = (status) => {
-  //   switch(true) {
-  //     case (status >= 300 && status <= 399):
-  //     return `${status}: Redirection message`
-  //     break;
-  //     case (status >= 400 && status <= 499):
-  //     return `${status}: Client error`
-  //     break;
-  //     case (status >= 500):
-  //     return `${status}: Server error`
-  //     break;
-  //     default:
-  //     return 'I have no idea what this error message is for...';
-  //   }
-  // }
-
   render() {
     return (
       <main className="App">
@@ -76,29 +30,6 @@ class App extends Component{
       </main>
     )
   }
-
-  // render() {
-  //   return (
-  //     <main>
-  //       <Navbar />
-  //       {this.state.errorMessage ?
-  //       <ErrorMessage
-  //           message={this.state.errorMessage}
-  //       />
-  //       :
-  //       this.state.movieSelected ?
-  //         <MovieDetails
-  //           movie={this.state.movieDetails}
-  //         />
-  //       :
-  //         <MoviesContainer
-  //           handleClick={this.handleClick}
-  //           movies={this.state.movies}
-  //         />
-  //       }
-  //     </main>
-  //   )
-  // }
 
 }
 
